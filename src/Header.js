c207ace337c79afca0461790b9d73d9002fff3de
Orunto Eniola <oruntoeniola@aol.com>
2022-05-20 14:56:42 +0100

    Let's start

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Hwader() {
+  return (
+      <header className="App-header">
+        <h1>Admin Dashboard</h1>
+      </header>
+  );
+}
+
+export default Header;
